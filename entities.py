8ac025f4b647ac95f125e62dd5a2895cc3c9e2d0
przemek.kobylas23 <przemek.kobylas23@gmail.com>
2026-01-19 15:36:47 +0000

    Content: Add restaurant descriptions and restaurant_info intent

--- entities.py
+++ entities.py
@@ -46,3 +46,9 @@ KW_RESTAURANTS = {
     "porto azzurro": "Porto Azzurro",
     "u włocha": "Porto Azzurro"
 }
+
+RESTAURANT_DESCRIPTIONS = {
+    "Neon": "Neon to pulsujące serce miejskiego smaku, gdzie energia miasta spotyka się z kreatywną kuchnią uliczną. Oferujemy rzemieślnicze burgery z lokalnej wołowiny, egzotyczne bao buns, chrupiące tacos z autorskimi sosami i innowacyjne dania wegańskie.",
+    "Porto Azzurro": "Porto Azzurro to kulinarna podróż do słonecznej Italii i Grecji, z nutą hiszpańskich smaków. W naszej karcie znajdziesz esencję kuchni śródziemnomorskiej: świeże owoce morza, ręcznie robione pasty, chrupiące pizze z pieca opalanego drewnem, soczyste mięsa z grilla i mnóstwo świeżych warzyw, ziół oraz najlepszej oliwy z oliwek.",
+    "Zielnik": "Zielnik to restauracja, w której tradycja spotyka się z nowoczesnością. Nasze menu opiera się wyłącznie na świeżych, sezonowych produktach od lokalnych dostawców. Szef kuchni czerpie inspiracje z bogactwa polskich pól i lasów, tworząc dania, które zaskakują smakiem i formą."
+}
